9347ecc84269057bdc649f2b5658cde7d2879e3d
kuldeep-tiwari <40053683+kuldeep-tiwari@users.noreply.github.com>
2018-06-08 07:26:48 +0000

    Update Tets.java

--- Tets.java
+++ Tets.java
@@ -22,7 +22,6 @@ if (args != null && args.length == 2) {
 inputFile = args[0];
 outputFile = args[1];
 }
-System.out.println("inputFile:" + inputFile + ",outputFile:" + outputFile);
 FileInputStream in = new FileInputStream(inputFile);
 XWPFDocument document = new XWPFDocument(in);
 File outFile = new File(outputFile);
